0d96974ecf7d6c9762cd639b29de22200ac10e1a
Rob Thuleen <rob.thuleen@gmail.com>
2016-04-13 13:05:00 -0700

    Added mjpeg route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.sendFile(__dirname + '/index.html');
 });
+
+router.get('/mjpeg/:timestamp', function (req, res, next) {
+	var timestamp = request.params.timestamp;
+	console.log("***** GOT MJPEG request for id " +   timestamp);
+
+
+
+})
 
 module.exports = router;
