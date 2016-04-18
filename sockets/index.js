998f8369db719f1db1db6ea48c7c08f50e4f0d59
Rob Thuleen <rob.thuleen@gmail.com>
2016-04-18 09:16:28 -0700

    Cleanup

--- sockets/index.js
+++ sockets/index.js
@@ -14,56 +14,38 @@ slack.setWebhook(process.env.SLACK_WEBHOOK_URL);
 var publicDir = "public";
 var imageDir= "images";
 
-function sendSlackAlert( source, imageUrl) {
-  console.log("Image Url = " + imageUrl);
-  slack.webhook({
-      text: "Motion Detected by " + source + "\n<" + imageUrl + "|Preview>"
-    }, 
-    function(err, response) {
-      console.log(response);
-    });
-}
-
-function init(server) {
+function controller(server) {
   var io = socketIo(server);
-  var controllerIo = io.of('/controller');
+  var clientIo = io.of('/controller');
   var cameraIo = io.of('/camera');  
+  var clientCount = 0;
+  clientIo.on('connection', clientConnected);
+  cameraIo.on('connection', cameraConnected);
 
-  var controllerCount = 0;
+  function sendSlackAlert( source, imageUrl) {
+    console.log("Image Url = " + imageUrl);
+    slack.webhook({
+        text: "Motion Detected by " + source + "\n<" + imageUrl + "|Preview>"
+      }, 
+      function(err, response) {
+        console.log(response);
+      });
+  }
 
-  controllerIo.on('connection', function (socket) {
+  function clientConnected(socket) {
     console.log("Controller connected...");
-    controllerCount++;
-    socket.on('activate', function(message) {
-      console.log("Got activate event");
-      var camera = Cameras.findById(message.id);
-      if (camera) {
-        camera.socket.emit('activate');
-      }
-      else {
-        console.log("Err- no such camera");
-        socket.send({err: "no such camera"})
-      }
-    });
-
-    socket.on('deactivate', function (message) {
-        console.log("got deactivate event");
-        var camera = Cameras.findById(message.id);
-        if (camera) {
-          camera.socket.emit('deactivate');
-        }
-        else {
-          console.log("Err- no such camera");
-          socket.send({err: "no such camera"})
-        }
-    });
-    socket.on('disconnect', function () {
-      controllerCount--;
-    })
+    clientCount++;
+    socket.on('activate', activateCamera);
+    socket.on('deactivate', deactivateCamera);
+    socket.on('disconnect', clientDisconnected)
     socket.emit('list', Cameras.list())
-  })
+  }
 
-  cameraIo.on('connection', function (socket) {
+  function clientDisconnected(socket) {
+    clientCount--;
+  }
+
+  function cameraConnected(socket) {
     console.log('Camera connected...');
     socket.on('register', registerCamera(socket));
     socket.on('status', updateStatus(socket));
@@ -72,14 +54,38 @@ function init(server) {
     socket.on('mjpeg', sendMjpeg(socket));
     socket.on('alarm', sendAlarm(socket));
     socket.on('disconnect', disconnectCamera(socket));
-  });
+  }
+
+  function activateCamera(message) {
+    console.log("Got activate event");
+    var camera = Cameras.findById(message.id);
+    if (camera) {
+      camera.socket.emit('activate');
+    }
+    else {
+      console.log("Err- no such camera");
+      socket.send({err: "no such camera"})
+    }
+  }
+
+  function deactivateCamera(message) {
+    console.log("got deactivate event");
+    var camera = Cameras.findById(message.id);
+    if (camera) {
+      camera.socket.emit('deactivate');
+    }
+    else {
+      console.log("Err- no such camera");
+      socket.send({err: "no such camera"})
+    }
+  }
 
   function sendMjpeg(socket) {
     return function (timestamp) {
       var camera = Cameras.findBySocket(socket);
       var url = "/mjpeg/" + timestamp;
       camera.setMjpegUrl(timestamp, url);
-      controllerIo.emit('mjpeg', {id: camera.id, alarmId: timestamp, src: url});
+      clientIo.emit('mjpeg', {id: camera.id, alarmId: timestamp, src: url});
     }
   }
 
@@ -99,9 +105,9 @@ function init(server) {
     return function (alarm) {
       var camera = Cameras.findBySocket(socket);
       camera.addAlarm({type: alarm.type, timestamp: alarm.timestamp})
-      controllerIo.emit('alarm', {id: camera.id, type: alarm.type, timestamp: alarm.timestamp});
+      clientIo.emit('alarm', {id: camera.id, type: alarm.type, timestamp: alarm.timestamp});
     }
-  };
+  }
 
   function sendImage (socket) {
     return function(stream, data, cb) {
@@ -118,7 +124,7 @@ function init(server) {
         return console.log("Error creating directories.");
       }
       stream.on('finish', function () {
-          controllerIo.emit('thumbnail', {id: camera.id, alarmId: data.timestamp, src: url});
+          clientIo.emit('thumbnail', {id: camera.id, alarmId: data.timestamp, src: url});
           sendSlackAlert(camera.name, "https://pi-control.herokuapp.com/" + url);
       });
       stream.on('error', function () {
@@ -126,15 +132,15 @@ function init(server) {
       });
       stream.pipe(fs.createWriteStream(fullPath, {mode: "0666"}));
     }
-  };
+  }
 
   function updateStatus (socket) {
     return function (message) {
       var camera = Cameras.findBySocket(socket);
       camera.status = message.status;
-      controllerIo.emit('status', {id: camera.id, status: camera.status})
+      clientIo.emit('status', {id: camera.id, status: camera.status})
     }
-  };
+  }
 
   function registerCamera (socket) {
     return function (camera) {
@@ -145,7 +151,7 @@ function init(server) {
       else {
         Cameras.add({name: camera.name, id: camera.id, socket: socket});
       }
-      controllerIo.emit('list', Cameras.list())
+      clientIo.emit('list', Cameras.list())
     }
   }
 
@@ -154,14 +160,14 @@ function init(server) {
       var camera = Cameras.findBySocket(socket);
       if (camera) {
         camera.setStatus('offline');
-        controllerIo.emit('status', {id: camera.id, status: camera.status})
+        clientIo.emit('status', {id: camera.id, status: camera.status})
       }
       else {
         console.log("Unable to find camera in collection");
       }
-      console.log("Camera disconnected...");
+      console.log("Camera disconnected");
     }
-  };
+  }
 
   function createThumbnailUrl(alarmId, filename) {
     var url = "/" + imageDir + "/capture/" + alarmId + "/" + filename;
@@ -169,5 +175,4 @@ function init(server) {
   }
 }
 
-
-module.exports = init;
+module.exports = controller;
