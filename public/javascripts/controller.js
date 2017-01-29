bab8db8e574d862683d035c9ceb2e6e4c9dcaa15
Rob Thuleen <rob.thuleen@gmail.com>
2017-01-29 10:31:52 -0800

    Added toolbar

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,30 +1,58 @@
-var controller = io.connect('/controller');
+var controller
 var cameraList = {};
 
 function listCameras(cameras) {
     $('#camera-list').remove();
-    var $cameraList = $("<div>", {id: "camera-list"});
+    var $cameraList = $("<div>", {
+        id: "camera-list"
+    });
     $('#cameras').append($cameraList);
 
     for (cameraId in cameras) {
         var $cameraRow = createCameraRow(cameraId);
-        $cameraList.append($cameraRow)  
-        updateCameraRow(cameraId);       
+        $cameraList.append($cameraRow)
+        updateCameraRow(cameraId);
     }
 
 }
 
 function createCameraRow(cameraId) {
-    var $cameraRow = $("<div>", {id:"camera-" + cameraId, class: "row"});
-    var $statusRow = $('<div>', {class: 'row'});
-    var $cameraName = $("<div>", {class: "col-xs-8 name"});
-    var $buttonCol = $("<div>", {class: "col-xs-4 col-sm-2 col-md-1 col-lg-1"});
-    var $speakRow = $("<div>", {class: 'row'})
-    var $speakText = $("<input>", {id: cameraId+ "-speak-text", type: "text"});
-    var $speakButton = $("<button>", {type: "button", 'data-camera-id': cameraId, text: "Speak"});
-    var $alarmsRow = $('<div>', {class: 'row'});
-    var $alarms = $('<div>', {class: 'col-xs-12 alarms'});
-    var $cameraButton = $("<button>", {type: "button", class: "status", 'data-camera-id': cameraId})
+    var $cameraRow = $("<div>", {
+        id: "camera-" + cameraId,
+        class: "row"
+    });
+    var $statusRow = $('<div>', {
+        class: 'row'
+    });
+    var $cameraName = $("<div>", {
+        class: "col-xs-8 name"
+    });
+    var $buttonCol = $("<div>", {
+        class: "col-xs-4 col-sm-2 col-md-1 col-lg-1"
+    });
+    var $speakRow = $("<div>", {
+        class: 'row'
+    })
+    var $speakText = $("<input>", {
+        id: cameraId + "-speak-text",
+        type: "text"
+    });
+    var $speakButton = $("<button>", {
+        type: "button",
+        'data-camera-id': cameraId,
+        text: "Speak"
+    });
+    var $alarmsRow = $('<div>', {
+        class: 'row'
+    });
+    var $alarms = $('<div>', {
+        class: 'col-xs-12 alarms'
+    });
+    var $cameraButton = $("<button>", {
+        type: "button",
+        class: "status",
+        'data-camera-id': cameraId
+    })
     $cameraButton.click(toggleStatus);
     $speakButton.click(sendSpeach);
     $buttonCol.append($cameraButton);
@@ -49,36 +77,53 @@ function updateCameraRow(cameraId) {
     $cameraRow.find('.name').text(camera.name);
     setStatus(camera.id, camera.status);
     $(id).find('.alarms').empty();
-    for (var i=camera.alarms.length-1; i>=0; i--) {
+    for (var i = camera.alarms.length - 1; i >= 0; i--) {
         setAlarm(camera.id, camera.alarms[i]);
     }
 }
 
 function toggleStatus() {
     var cameraId = this.dataset.cameraId;
-    var event = (cameraList[cameraId].status=='active')?'deactivate':'activate';
-    controller.emit(event, {id: cameraId});
+    var event = (cameraList[cameraId].status == 'active') ? 'deactivate' : 'activate';
+    controller.emit(event, {
+        id: cameraId
+    });
     return false
 };
 
 function sendSpeach() {
     var cameraId = this.dataset.cameraId;
-    var textElement = $('#'+cameraId+"-speak-text");
+    var textElement = $('#' + cameraId + "-speak-text");
     var text = textElement[0].value;
     console.log("Sending speech text: " + text)
-    controller.emit('speak', {id: cameraId, text:text})
+    controller.emit('speak', {
+        id: cameraId,
+        text: text
+    })
 }
 
 
 
 function setAlarm(cameraId, alarm) {
-    var id = "#camera-"+cameraId;
+    var id = "#camera-" + cameraId;
     var $alarms = $(id).find('.alarms');
-    var $alarmItem = $('<div>', {id: "alarm-" + cameraId + "-" + alarm.timestamp, class: 'row alarm'});
-    var $alarmText = $('<div>', {class: 'col-xs-8 timestamp',  text:  moment.utc(alarm.timestamp, "YYYYMMDDHHmmss").local().format('MMM Do YYYY HH:mm:ss')})
-    var $alarmImgCol = $('<div>', {class: 'col-xs-4'});
-    var $alarmImgLink = $('<a>', {class: 'mjpeg-link'});
-    var $alarmImg = $("<img>", {class: "alarm-image thumbnail"});
+    var $alarmItem = $('<div>', {
+        id: "alarm-" + cameraId + "-" + alarm.timestamp,
+        class: 'row alarm'
+    });
+    var $alarmText = $('<div>', {
+        class: 'col-xs-8 timestamp',
+        text: moment.utc(alarm.timestamp, "YYYYMMDDHHmmss").local().format('MMM Do YYYY HH:mm:ss')
+    })
+    var $alarmImgCol = $('<div>', {
+        class: 'col-xs-4'
+    });
+    var $alarmImgLink = $('<a>', {
+        class: 'mjpeg-link'
+    });
+    var $alarmImg = $("<img>", {
+        class: "alarm-image thumbnail"
+    });
     $alarmImgLink.append($alarmImg);
     $alarmItem.append($alarmText);
     $alarmItem.append($alarmImgCol);
@@ -96,18 +141,18 @@ function setThumbnail(cameraId, alarmId, src) {
     var id = "#alarm-" + cameraId + "-" + alarmId;
     $(id).find(".alarm-image").error(function (e) {
         console.log('image error');
-    }).attr("src",src)
+    }).attr("src", src)
 }
 
-function setMjpeg (cameraId, alarmId, url) {
+function setMjpeg(cameraId, alarmId, url) {
     var id = '#alarm-' + cameraId + "-" + alarmId;
     var link = $(id).find(".mjpeg-link");
-    link.attr("href",url);
+    link.attr("href", url);
     link.attr("target", "_blank");
 }
 
 function setStatus(cameraId, status) {
-    var id = "#camera-"+cameraId;
+    var id = "#camera-" + cameraId;
     cameraList[cameraId].status = status;
     var buttonClasses = {
         'offline': {
@@ -133,31 +178,48 @@ function setStatus(cameraId, status) {
     $statusBtn.attr('class', btnClass);
 }
 
-/**
- * Handle socket events
- */
-controller
-    .on('connect', function(socket) {
-        console.log("Controller connected");
-    })
-    .on('status', function (status) {
-        console.log("Got status for " + status.id + " - " + status.status)
-        setStatus(status.id, status.status);
-    })
-    .on('alarm', function (alarm) {
-        console.log("Got alarm signal for camera " + alarm.id + " - " + alarm.type);
-        setAlarm(alarm.id, alarm);
-    })
-    .on('thumbnail', function (image) {
-        console.log("Got image from camera " + image.id);
-        // set thumbnail to image
-        setThumbnail(image.id, image.alarmId, image.src);
-    })
-    .on('list', function (cameras) {
-        cameraList = cameras;
-        listCameras(cameras);
-    })
-    .on('mjpeg', function (url) {
-        setMjpeg(url.id, url.alarmId, url.src);
-        console.log("Got MJPEG URL " + url);
-    })
+function logout() {
+    window.location = '/logout'
+}
+
+function settings() {
+    alert("settings");
+}
+//
+// When loaded, set up app events
+//
+$(document).ready(function () {
+    $('#logout').on('click', logout);
+    $('#settings').on('click', settings);
+
+    /**
+     * Handle socket events
+     */
+    controller = io.connect('/controller');
+    controller
+        .on('connect', function (socket) {
+            console.log("Controller connected");
+        })
+        .on('status', function (status) {
+            console.log("Got status for " + status.id + " - " + status.status)
+            setStatus(status.id, status.status);
+        })
+        .on('alarm', function (alarm) {
+            console.log("Got alarm signal for camera " + alarm.id + " - " + alarm.type);
+            setAlarm(alarm.id, alarm);
+        })
+        .on('thumbnail', function (image) {
+            console.log("Got image from camera " + image.id);
+            // set thumbnail to image
+            setThumbnail(image.id, image.alarmId, image.src);
+        })
+        .on('list', function (cameras) {
+            cameraList = cameras;
+            listCameras(cameras);
+        })
+        .on('mjpeg', function (url) {
+            setMjpeg(url.id, url.alarmId, url.src);
+            console.log("Got MJPEG URL " + url);
+        })
+
+})
--- routes/login.js
+++ routes/login.js
@@ -3,20 +3,19 @@ var passport = require('passport');
 module.exports = login;
 
 function login(req, res, next) {
-    passport.authenticate('local', function (err, user, info) {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            return res.status(401).json({
-                errors: [info.message],
-                data: {}
-            })
-        }
+    passport.authenticate('local', sendResponse)(req, res, next);
+
+    function sendResponse(err, user, info) {
+        if (err) return next(err);
+        // If not authenticated
+        if (!user) return res.status(401).json({
+            errors: [info.message],
+            data: {}
+        })
+
+        // Authenticated - Log in user to the session
         req.logIn(user, function (err) {
-            if (err) {
-                return next(err);
-            }
+            if (err) return next(err);
             return res.json({
                 errors: [],
                 data: {
@@ -24,5 +23,5 @@ function login(req, res, next) {
                 }
             })
         });
-    })(req, res, next);
+    };
 };
