3a8318f4ed4f5f7b8773669e0b9f1f7bfcaf0c07
Rob Thuleen <rob.thuleen@gmail.com>
2017-01-28 18:38:18 -0800

    Added logout route

--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,14 @@ router.get('/', auth.isLoggedIn, function (req, res, next) {
 	res.render('index');
 });
 
+//Logout
+router.get('/logout', function (req, res) {
+	req.logout();
+	res.redirect('/login');
+});
+
 // API
 router.post('/login', login)
-router.get('/mjpeg/:timestamp', auth.isLoggedIn, mjpeg)
+router.get('/mjpeg/:timestamp', auth.isLoggedIn, mjpeg);
 
 module.exports = router;
